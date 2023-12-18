6519485b62945e55bfe89b44bdba53f5f164db65
Magnus <iwarrior.xr@gmail.com>
2023-12-18 12:43:44 +0800

    init

--- fileHandler.go
+++ fileHandler.go
@@ -5,6 +5,9 @@ import (
 )
 
 var fileHandler = func(resp http.ResponseWriter, req *http.Request) {
+	resp.WriteHeader(200)
+	resp.Write([]byte("hahahaha"))
+	return
 	if len(req.Header.Get("X-ENV-WEB")) > 0 && len(req.Header.Get("X-Redirect")) == 0 {
 		env := req.Header.Get("X-ENV-WEB")
 		http.FileServer(http.Dir("/root/sharedNoteWeb"+env)).ServeHTTP(resp, req)
